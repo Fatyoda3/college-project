4cef6ea6188e1601a6c9356fc76efce7520a648a
FAT YODA <shivangsingh654@gmail.com>
2025-04-26 14:37:58 +0530

    just making admin login

--- index.js
+++ index.js
@@ -17,17 +17,34 @@ server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 server.use(express.static(path.join(__dirname, 'public'))); // literally just a folder with static files
 
-server.post('/api/bookings', (req, res) => {
-	const { name, email, phone, checkin, checkout, roomtype, guests } =
-		req.body;
-	console.log(name, email, phone, checkin, checkout, roomtype, guests);
-	const countPath = path.join(__dirname, 'bookingsCount.txt');
-	const count = fs.readFile(countPath);
-	console.log(count);
+server.post('/api/bookings', async (req, res) => {
+	/* const { name, email, phone, checkin, checkout, roomtype, guests } =
+		req.body; */
+	// console.log(name, email, phone, checkin, checkout, roomtype, guests);
+
+	const countPath = path.join(__dirname, 'bookingCount.txt');
+
+	let count = await fs.readFile(countPath, 'utf8');
+
+	console.log((count = Number(count)));
 
 	const bookingPath = path.join(__dirname, 'bookings');
+
+	const CurrBooking = path.join(bookingPath, `${count + '.json'}`);
+
+	fs.writeFile(CurrBooking, JSON.stringify(req.body));
+
+	fs.writeFile(countPath, String((count += 1)));
+
+
+	res.send('console.log("booked")')
 });
 
+
+server.post ('/api/admin/login' , async(req ,res )=>{
+
+	
+})
 server.listen(PORT, () => {
 	console.log('Server is running on ' + PORT);
 });

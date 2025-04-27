9cd3c940585f2e6286251e02a4f6926b83dd0194
FAT YODA <shivangsingh654@gmail.com>
2025-04-27 12:45:13 +0530

    compressed some images doing some finishing

--- public/scripts/hotel-details.js
+++ public/scripts/hotel-details.js
@@ -1,65 +1,71 @@
+const hotels = [
+	{
+		id: 1,
+		name: 'Iconic',
+		address: '1903 St, LaSanta Alley, 21',
+		price: '$1000',
+		rating: 4.48,
+		image: '../Hotel-Bedroom/Image 1.jpg',
+	},
+	{
+		id: 2,
+		name: 'Glossy Twin Villa',
+		address: '148 St, Locho Alley, 11028',
+		price: '$3,499',
+		rating: 5.0,
+		image: '../Hotel-Bedroom/Image 2.jpg',
+	},
+	{
+		id: 3,
+		name: 'Palmasantos Hosting House',
+		address: '172 St, Palma Alley, 10911',
+		price: '$949',
+		rating: 4.96,
+		image: '../Hotel-Bedroom/Image 4.jpg',
+	},
+];
+/* 	let id; */
+let id = 0;
 document.addEventListener('DOMContentLoaded', function () {
-	console.log();
-
-	const hotels = [
-		{
-			id: 1,
-			name: 'Iconic',
-			address: '1903 St, LaSanta Alley, 21',
-			price: '$1000',
-			rating: 4.48,
-			image: '../Hotel-Bedroom/Image 1.jpg',
-		},
-		{
-			id: 2,
-			name: 'Glossy Twin Villa',
-			address: '148 St, Locho Alley, 11028',
-			price: '$3,499',
-			rating: 5.0,
-			image: '../Hotel-Bedroom/Image 2.jpg',
-		},
-		{
-			id: 3,
-			name: 'Palmasantos Hosting House',
-			address: '172 St, Palma Alley, 10911',
-			price: '$949',
-			rating: 4.96,
-			image: '../Hotel-Bedroom/Image 4.jpg',
-		},
-		{
-			id: 1,
-			name: 'Individual Modern House',
-			address: '1903 St, LaSanta Alley, 21',
-			price: '$1,099',
-			rating: 4.48,
-			image: '../Hotel-Bedroom/Image 2.jpg',
-		},
-	];
-	let id;
-	let t = new URLSearchParams(this.URL);
-	for (const key in t) {
-		id = t[key];
-	}
+	let t = new URLSearchParams(new URL(this.URL).search); //new URLSearchParams(new URL(url).search);
+	// console.log(t['id']);
+	// console.log(t);
+	/* let  */ id = Number(t.get('id')) - 1;
 	let currHotel = hotels[id];
+	console.table(currHotel, hotels, t, id);
+	//documenting so sometimes we forget indexes start from 0 so be careful folks
 	document.querySelector('.hero-image').src = currHotel.image;
 	document.querySelector('.hotel-name').textContent = currHotel.name;
 	document.querySelector('.hotel-address').textContent = currHotel.address;
 	document.querySelector('.hotel-rating').textContent = currHotel.rating;
 	document.querySelector('.hotel-price').textContent = currHotel.price;
-	document.querySelector('.hotel-description').textContent =
+	/* document.querySelector('.hotel-description').textContent =
 		currHotel.description;
 	document.querySelector('.hotel-details-text').textContent =
-		currHotel.details;
+		currHotel.details; */
 
 	document
 		.querySelector('.book-now-button')
 		.addEventListener('click', function () {
 			alert('Booking process for ' + currHotel.name);
 		});
+
+	const images = document.querySelectorAll('.a1');
+	console.log(images);
+
+	images.forEach((el) => {
+		let path = '../Hotel-Bedroom/';
+		//Image 4(4).jpg
+
+		/* document.getElementById('hero-image') .src  */
+		el.src = path + `Image ${id + 1}(${el.id}).jpg`;
+	});
 });
 
-// for image swapping :----
-function swapHeroImage(src) {
-	document.getElementById('hero-image').src = src;
-	console.log(this);
+function swapHeroImage(n) {
+	let path = '../Hotel-Bedroom/';
+	//Image 4(4).jpg
+
+	document.getElementById('hero-image').src =
+		path + `Image ${id + 1}(${n}).jpg`;
 }
--- public/scripts/viewHotels.js
+++ public/scripts/viewHotels.js
@@ -23,6 +23,30 @@ const hotels = [
         rating: 4.96,
         image: "../Hotel-Bedroom/Image 4.jpg"
     },
+    {
+        id: 1,
+        name: "Iconic",
+        address: "1903 St, LaSanta Alley, 21",
+        price: "$1000",
+        rating: 4.48,
+        image: "../Hotel-Bedroom/Image 1.jpg"
+    },
+    {
+        id: 2,
+        name: "Glossy Twin Villa",
+        address: "148 St, Locho Alley, 11028",
+        price: "$3,499",
+        rating: 5.00,
+        image: "../Hotel-Bedroom//Image 2.jpg"
+    },
+    {
+        id: 3,
+        name: "Palmasantos Hosting House",
+        address: "172 St, Palma Alley, 10911",
+        price: "$949",
+        rating: 4.96,
+        image: "../Hotel-Bedroom/Image 4.jpg"
+    },
   
 ];
 
